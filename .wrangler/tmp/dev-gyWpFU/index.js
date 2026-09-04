6b2f9d30d8e4b6e2cdc734bc60d03aeb0fd2f4b0
teamwatcher25-stack <teamwatcher25@gmail.com>
2026-09-04 21:53:00 +0100

    Update Watcher Platform

--- .wrangler/tmp/dev-gyWpFU/index.js
+++ .wrangler/tmp/dev-gyWpFU/index.js
@@ -6,7 +6,7 @@ var __publicField = (obj, key, value) => {
   return value;
 };
 
-// .wrangler/tmp/bundle-BrEF72/strip-cf-connecting-ip-header.js
+// .wrangler/tmp/bundle-qyNVg3/strip-cf-connecting-ip-header.js
 function stripCfConnectingIPHeader(input, init) {
   const request = new Request(input, init);
   request.headers.delete("CF-Connecting-IP");
@@ -3678,7 +3678,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env2, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-BrEF72/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-qyNVg3/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -3710,7 +3710,7 @@ function __facade_invoke__(request, env2, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-BrEF72/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-qyNVg3/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
